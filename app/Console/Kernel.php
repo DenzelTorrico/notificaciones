4490ce8053441ca79370a2547e8239ec35aa8165
DenzelTorrico <denzel_silva@hotmail.com>
2024-01-13 00:46:43 -0500

    backuprun

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,10 +13,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         //$schedule->command('backup:clean')->daily()->at('02:05');
-        //$schedule->command('backup:run')->daily()->at('02:05');
-        $schedule->call(function () {
-            \Log::info('Log command executed every second.');
-        })->everySecond();
+        $schedule->command('backup:run')->everyMinute()->then(function(){
+            \Log::info('Backup creation task executed.');
+        });
+        //$schedule->call(function () {
+          //  \Log::info('Log command executed every second.');
+        //})->everySecond();
 
         /// $schedule->command('inspire')->hourly();
     }
